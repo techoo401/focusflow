1afdea42804e637d9f0f83dc428908363ca430d6
Himanshu <himanshufromxt@gmail.com>
2025-04-07 08:55:22 +0530

    days row updated

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react"
 import gsap from "gsap"
 
-function Day () {
+function Day() {
     const [currentDate, setCurrentDate] = useState(new Date())
     const dayRefs = useRef([])
 
@@ -15,6 +15,20 @@ function Day () {
     }, [])
 
     const weekday = currentDate.toLocaleDateString("en-US", { weekday: "short" });
+    const weekdates = () => {
+        const startOfWeek = new Date(currentDate)
+        startOfWeek.setDate(currentDate.getDate() - currentDate.getDay()) //return to sunday
+
+        return Array.from({ length: 7 }, (_, i) => {
+            const date = new Date(startOfWeek)
+            date.setDate(startOfWeek.getDate() + i)
+            return {
+                date: date.getDate(),
+                month: date.toLocaleDateString("en-US", { month: "short" }),
+                day: date.toLocaleDateString("en-US", { weekday: "short" })
+            }
+        })
+    }
 
     // Animate only current day
     useEffect(() => {
@@ -30,17 +44,29 @@ function Day () {
 
     return (
         <div className="flex items-center mt-5 justify-evenly">
-            {["Sun","Mon","Tue","Wed","Thu","Fri","Sat"].map((day, index) => (
-                <div
-                    ref={el => dayRefs.current[index] = el}
-                    className={`w-20 h-30 px-20 py-20 flex items-center justify-center text-4xl rounded font-semibold ${
-                        day === weekday ? "text-white bg-blue-500" : "text-black bg-blue-100"
-                    }`}
-                    key={index}
-                >
-                    {day}
-                </div>
-            ))}
+            {weekdates().map((value, index) => {
+                return (
+                    <div
+                        className={`w-20 h-30 px-20 py-20 flex flex-col items-center justify-center rounded font-semibold ${value.day === weekday ? "text-white bg-blue-500" : "text-black bg-blue-100"
+                            }`}
+                        ref={el => dayRefs.current[index] = el}
+                        key={index}>
+
+                        <div
+                            className="text-4xl">
+                            {value.day}
+                        </div>
+
+                        <div className="text-xl mt-5">
+                            {value.date}
+                        </div>
+
+                        <div>
+                            {value.month}
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
